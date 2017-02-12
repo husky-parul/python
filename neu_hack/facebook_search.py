60df041d5d7ad1fda7bd1fdeaee57aeeb615caae
husky-parul <promptparul@gmail.com>
2017-02-12 02:17:27 -0500

    version with sort on checkins

--- neu_hack/facebook_search.py
+++ neu_hack/facebook_search.py
@@ -1,6 +1,14 @@
 import json
 import urllib
 
+class Events(object):
+	description=''
+	end_time=''
+	name=''
+	place=''
+	start_time=''
+	address=''
+
 class Result(object):
 	about=''
 	category=''
@@ -9,25 +17,53 @@ class Result(object):
 	phone=''
 	price=''
 	website=''
+	checkins=''
+	events=Events()
+	
+
 
 class SearchApi(object):
 	id_list=[]
 	page=0
+	access='EAACEdEose0cBAAzbZB8HaGMDfEVaLuWZAZAa6nt6QzdL53kqZChIHVPapnZC3sa2TJLyqZC3recb7AJD8mfxatxmuGBpAcG1XOhYEAAg1oSZAIGM78ZA2eUYtoy4AqQScKg0wHEZAmGEnTky994c5diZCcRz3D1zhh2RWB9HnRSZAASTpc45ety4AnK'
 	
 	def make_request(self):
 		latt=42.353904
 		long=-71.133711
-		
 		data = {}
 		data['type'] = 'place'
 		data['distance'] = 4000
-		data['access_token'] = 'EAACEdEose0cBAGn0mKHlLbM3KuqDx8p3PGdK8EAhNeCood7Ib05fv5Rk2Fa5UeE6QZAcyDmIRW1mj3lZBaISDP2tLLkhel3LpkTlzVuDuOgKwhchZAd50hxZCUD72wWzvj85YNlDoFWzalVnFLl0629770tIyZCi9iqjZCLZCLOnvqlE8XPZChkw'
+		data['access_token'] = self.access
 		url = "https://graph.facebook.com/v2.8/search"
 		url_values = urllib.urlencode(data)
 		new_url= url + "?" + url_values+"&center=42.353904%2c-71.133711"
 		response=urllib.urlopen(new_url)
 		result=self.get_data(new_url)
 		return self.get_data_for_id()
+		
+	
+		
+		
+	def get_event_data(self,id):
+		event_list=[]
+		url = 'https://graph.facebook.com/v2.8/'
+		extra=id+'/events?limit=10&access_token='+self.access
+		url=url+extra
+		
+		response=urllib.urlopen(url)
+		result=json.load(response)
+		for i in result['data']:
+			event=Events()
+			
+			event.description=i.get('description', '')
+			event.end_time=i.get('end_time', '')
+			event.name=i.get('name', '')
+			event.place=i.get('place', '')
+			event.address=i.get('state', '')+' '+i.get('street', '')+' '+i.get('zip', '')
+			event.start_time=i.get('start_time', '')
+			event_list.append(event)
+		return event_list
+	
 		
 		
 	def get_data(self,url):
@@ -54,25 +90,29 @@ class SearchApi(object):
 
 	def get_data_for_id(self):
 		result_list=[]
-		access = 'EAACEdEose0cBAGn0mKHlLbM3KuqDx8p3PGdK8EAhNeCood7Ib05fv5Rk2Fa5UeE6QZAcyDmIRW1mj3lZBaISDP2tLLkhel3LpkTlzVuDuOgKwhchZAd50hxZCUD72wWzvj85YNlDoFWzalVnFLl0629770tIyZCi9iqjZCLZCLOnvqlE8XPZChkw'
+		access = self.access
 		url = 'https://graph.facebook.com/v2.8/'
-		extra='?fields=about,business,category,description,features,hours,phone,price_range,website&access_token='
+		extra='?fields=about,business,category,description,features,hours,phone,price_range,website,checkins&access_token='
 		for i in self.id_list:
 			newurl=url+str(i)+extra+access
 			response=urllib.urlopen(newurl)
 			result=json.load(response)
-			result_list.append(self.extract_data(result))
+			result_obj=self.extract_data(result)
+			result_obj.events=self.get_event_data(i)
+			result_list.append(result_obj)
+		result_list.sort(key=lambda x: x.checkins, reverse=True)
 		return result_list
 		
 			
 	def extract_data(self,data):
 		obj=Result()
-		obj.about=data.get('about', [])
-		obj.category=data.get('category', [])
-		obj.des=data.get('des', [])
-		obj.hours=data.get('hours', [])
-		obj.price=data.get('price', [])
-		obj.website=data.get('website', [])
+		obj.about=data.get('about', '')
+		obj.category=data.get('category', '')
+		obj.des=data.get('des', '')
+		obj.hours=data.get('hours', '')
+		obj.price=data.get('price', '')
+		obj.website=data.get('website', '')
+		obj.checkins=data.get('checkins', '')
 		return obj
 		
 		
