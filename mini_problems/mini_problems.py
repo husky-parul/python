fea2dd1673c556a050d0bcacdd6baa0e0e7788fb
husky-parul <promptparul@gmail.com>
2017-02-17 02:02:01 -0500

    Solving random small questions for practice and fun!

--- mini_problems/mini_problems.py
+++ mini_problems/mini_problems.py
@@ -11,4 +11,60 @@ def rotate(arr,k):
 	print last
 	print first+last
 	
-rotate([1,2,3,4,5,6,7],3)
+
+
+# Given two strings s and t, determine if they are isomorphic. Two strings are isomorphic
+# if the characters in s can be replaced to get t. For example,"egg" and "add" are 
+# isomorphic, "foo" and "bar" are not.
+
+def isomorphic(s1,s2):
+	lookups1={}
+	if len(s1)!= len(s2):
+		return 'no'
+	count=0
+	for i in s1:
+		if lookups1.get(i) !=None:
+			j,val=lookups1.get(i)
+			lookups1.update({i:(j,val+1)})
+		else:
+			lookups1.update({i:(s2[count],1)})
+		count+=1
+	print lookups1
+	lookups2={}
+	count=0
+	for i in s2:
+		if lookups2.get(i) !=None:
+			j,val=lookups2.get(i)
+			lookups2.update({i:(j,val+1)})
+		else:
+			lookups2.update({i:(s1[count],1)})
+		count+=1	
+	print lookups2
+	for key,val in lookups1.iteritems():
+		
+		val2=lookups2.get(val[0])
+		print val, val2, val2[1] , val[1]
+		if val2[1]!=val[1]:
+			print 'not isomorphic'
+			return
+	print 'isomorphic'
+	
+#Given an array of integers, find two numbers such that they add up to a specific target
+#number.
+
+def twoSumUsingHT(l,sum):
+	lookup={}
+	result=set()
+	for n in l:
+		lookup.update({n:sum-n})
+	for n in l:
+		if lookup.get(n) in l:
+			result.update([(n,sum-n)])
+	for n in result:
+		print n
+	
+
+twoSumUsingHT([-1,0,1,2,-1,-4],0)
+						
+#isomorphic('paper','title')
+#rotate([1,2,3,4,5,6,7],3)
