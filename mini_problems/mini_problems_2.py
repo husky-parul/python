bc2b0f121900ece03944b55db533d6ff9b688417
husky-parul <promptparul@gmail.com>
2017-02-21 14:45:48 -0500

    solving questions for practive and fun  on 2017-02-21 14:45:48

--- mini_problems/mini_problems_2.py
+++ mini_problems/mini_problems_2.py
@@ -10,7 +10,7 @@ def closetSum(A,target):
 			if diff==0:
 				return sum
 			if diff<min:
-				diff=min
+				min=diff
 				result=sum
 			if sum<target:
 				j+=1
@@ -18,5 +18,88 @@ def closetSum(A,target):
 				k-=1
 	return result
 	
+#print closetSum([-4,-1,1,2],1)	
 
-print closetSum([-4,-1,1,2],1)			
+'''
+Maximum Subarray Sum
+'''
+
+def maxSubarraySum(l):
+	#print '----------------'
+	n=len(l)
+	if n==1:
+		return l[0]
+	m=n/2
+	#print 'l[:m]: ',l[:m]
+	#print 'r[m:]: ',l[m:]
+	left_arr=maxSubarraySum(l[:m])
+	right_arr=maxSubarraySum(l[m:])
+	sum=0
+	rsum=float('-inf')
+	lsum=float('-inf')
+	for i in range (m,n):
+		sum+=l[i]
+		rsum=max(rsum,sum)
+	sum=0
+	#print 'rsum: ',rsum
+	for i in range (m-1,-1,-1):
+		sum+=l[i]
+		lsum=max(lsum,sum)
+	#print 'lsum: ',lsum
+	ans=max(right_arr,left_arr)
+	#print 'ans: ',ans
+	#print 'lsum+rsum: ',lsum+rsum
+	return max(ans,lsum+rsum)
+	
+print maxSubarraySum([-2,-3,4,-1,-2,1])
+print maxSubarraySum([-2,3,2,-1])
+print maxSubarraySum([-1, 4, -2, 5, -5, 2, -20, 6])
+print maxSubarraySum([-10, -4, -2, -5, -5, -1, -20, -6])
+print maxSubarraySum([1,-3,2,-5,7,6,-1,-4,11,-23])
+print ('-------------------------------------------------')
+'''
+Maximum Subarray Sum
+Does not work for negative numbers
+Time complexity O(n)
+'''
+def maxSubarraySumKadane(l):
+	max_so_far=max_ends_here=0
+	for i in l:
+		max_ends_here=max_ends_here+i
+		if max_ends_here<0:
+			max_ends_here=0
+		if max_so_far<max_ends_here:
+			max_so_far=max_ends_here
+	return max_so_far
+
+#print maxSubarraySumKadane ([2,-3,4,-1,2,1,5,-3])
+#print maxSubarraySumKadane([-2,-3,4,-1,-2,1])
+#print maxSubarraySumKadane([-2,3,2,-1])
+#print maxSubarraySumKadane ([1,-3,2,1,-1])
+
+'''
+Maximum Subarray Sum
+Does  work for negative numbers
+Time complexity O(n)
+'''
+def maxSubarraySumKadane_(l):
+	max_so_far=max_ends_here=0
+	max_val=float('-inf')
+	for i in l:
+		max_ends_here=max_ends_here+i
+		max_val=max(i,max_val)
+		if max_ends_here<0:
+			max_ends_here=0
+		if max_so_far<max_ends_here:
+			max_so_far=max_ends_here
+	if max_so_far==0:
+		return max_val
+	else:
+		return max_so_far
+
+print maxSubarraySumKadane_([-2,-3,4,-1,-2,1])
+print maxSubarraySumKadane_([-2,3,2,-1])
+print maxSubarraySumKadane_([-1, 4, -2, 5, -5, 2, -20, 6])
+print maxSubarraySumKadane_([-10, -4, -2, -5, -5, -1, -20, -6])
+print maxSubarraySumKadane_([1,-3,2,-5,7,6,-1,-4,11,-23])
+
